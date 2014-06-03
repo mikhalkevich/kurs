054a01ef521ecb5f5b40ade3214312709564689c
mikhalkevich <mikhalkevich@ya.ru>
2014-06-03 16:49:46 +0300

    База

--- routes/chat.js
+++ routes/chat.js
@@ -2,6 +2,7 @@ exports.index = function(req, res, next) {
  congif = require('../config');
  scripts = congif.get("scripts");
  scripts[3] = '/vendor/bower_components/jquery/jquery.js';
+ scripts[4] = '/vendor/bower_components/socket.io-client/dist/socket.io.js';
 
   res.render('chat', {
    scripts: scripts
